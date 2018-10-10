b7bae2f5a6fb261ca167179538da84f1030ab117
Konrad <ladakonrad@gmail.com>
2018-10-10 20:29:15 +0200

    dodano zadanie27

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function dluzszyString(nazwa1, nazwa2) {
+    let nazwa1Length = nazwa1.length;
+    let nazwa2Length = nazwa2.length;
+    if (nazwa1Length > nazwa2Length)
+        console.log(nazwa1)
+    else
+        console.log(nazwa2)
+}
+
+dluzszyString('Uwielbiam JavaScript', 'Jestem świetnym programistą');
