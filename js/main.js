beeb60bef5821ee194f89459e7f5575ca309d8a2
Konrad <ladakonrad@gmail.com>
2018-10-15 21:49:29 +0200

    poprawiono zadanie27js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ function dluzszyString(nazwa1, nazwa2) {
     let nazwa1Length = nazwa1.length;
     let nazwa2Length = nazwa2.length;
     if (nazwa1Length > nazwa2Length)
-        console.log(nazwa1)
+        return (nazwa1);
     else
-        console.log(nazwa2)
+        return (nazwa2);
 }
 
-dluzszyString('Uwielbiam JavaScript', 'Jestem świetnym programistą');
+console.log(dluzszyString('Uwielbiam JavaScript', 'Jestem świetnym programistą'));
